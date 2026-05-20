e1bb8750f09ef2e9d43a2ffe289e7b723d54c280
flaviodw <fgfdesigner@gmail.com>
2026-05-20 19:11:55 -0300

    Delete app/(tabs)/page.tsx

--- app/(tabs)/page.tsx
+++
@@ -1,9 +0,0 @@
-import AppContainer from '@/components/AppContainer';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-slate-100 flex items-center justify-center p-0 md:p-4">
-      <AppContainer />
-    </main>
-  );
-}
