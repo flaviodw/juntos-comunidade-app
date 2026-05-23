c72d2b4443cf60bde50a0dd24a8decf011a2ba13
flaviodw <fgfdesigner@gmail.com>
2026-05-23 14:12:33 -0300

    Remove tailwindcss plugin from PostCSS config
    Removed tailwindcss plugin to avoid conflicts.

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    // Removemos a linha do tailwindcss: {} daqui para evitar o conflito
   },
 };
 
